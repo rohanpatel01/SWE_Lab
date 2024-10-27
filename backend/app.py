fadabf979f98fe1b948895d46c2f014e7c83e660
Bryan <bryan.doan45@gmail.com>
2024-10-27 17:55:13 -0500

    changed static file path

--- backend/app.py
+++ backend/app.py
@@ -12,16 +12,13 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['SWELAB']
 collection = db['Users']
 
-app = Flask(__name__, static_folder='../fronend/build/static', static_url_path='/')
+app = Flask(__name__, static_folder='../fronend/build', static_url_path='/')
 CORS(app)  # Enable CORS for all routes
 
 # Render the main page
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        return redirect("/")
-    else:
-        return render_template("index.html")
+    return send_from_directory(app.static_folder, 'index.html')
 
 # Render the main page
 # @app.route("/", methods=["GET", "POST"])
